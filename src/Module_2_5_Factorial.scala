2ac508498a6efdabb6fbdfdbb63eb37cd85d8ea1
edureka <edureka@localhost.localdomain>
2019-02-23 18:08:42 +0530

    Added Module_2_5

---
+++ src/Module_2_5_Factorial.scala
@@ -0,0 +1,14 @@
+
+
+object Factorial {
+  def main(args: Array[String]) {
+    fact(3)
+  }
+  def fact(n: Int) {
+    var a = List.range(1,n+1)
+    println(a)
+    
+    var f = a.reduceLeft(_ * _)
+    println(f)
+  }
+}
